4c9f157699834a8cbe94ceffeffab113b6972a5e
naserdajani <89731778+naserdajani@users.noreply.github.com>
2023-01-16 10:20:07 +0300

    If error message happens

---
+++ FinalProject.py
@@ -0,0 +1,5 @@
+import sys
+
+if len(sys.argv) == 1:
+# giving the user a usage error message if they do not have any mode seected
+print('Usage: python3 Pro.py "Mode". \n To view all the options use list instead of mode\n')
