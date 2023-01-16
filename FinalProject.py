0d3359953c09df9790aefe34279a2bb3051481ed
naserdajani <89731778+naserdajani@users.noreply.github.com>
2023-01-16 11:29:56 +0300

    Making the command line argument while adding the list of equations i will use

--- FinalProject.py
+++ FinalProject.py
@@ -2,4 +2,15 @@ import sys
 
 if len(sys.argv) == 1:
 # giving the user a usage error message if they do not have any mode seected
-print('Usage: python3 Pro.py "Mode". \n To view all the options use list instead of mode\n')
+    print('Usage: python3 Pro.py "Mode". \n To view all the options use list instead of mode\n')
+
+def main():
+# make mode command line argument 
+mode = sys.argv[1]
+
+# putting all equations into proper casing
+if mode == 'list'
+    print('\n 1. acceleration\n 2. velocity\n 3. root2\n 4 rootx\n 5. force\n 6. power\n 7. density\n 8. potential_energy or PE\n 9. kinetic_energy or KE\n 10.')
+    print('Use mode: equations for a full list of all equations')
+    print(' ** Please use same casing\n')
+
